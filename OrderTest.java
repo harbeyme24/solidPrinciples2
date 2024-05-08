11e0e237c91634211036c07d75b8b67e379c39cd
Harbey M. Edroso <143273418+harbeyme24@users.noreply.github.com>
2024-05-08 23:05:50 +0800

    Update OrderTest.java

--- OrderTest.java
+++ OrderTest.java
@@ -2,13 +2,10 @@ public class OrderTest {
     public static void main(String[] args) {
         OrderAction order = new OrderAction();
 
-        //Vince Ted Matthew D. Chua
-        //May 5th, 2024
-        //Laboratory Assignment 5: SOLID Principle Assignment WK (14-15)
 
         order.calculateTotal(10.0, 2);
-        order.placeOrder("John Doe", "123 Main St");
+        order.placeOrder("Edroso", "123 Main St");
         order.generateInvoice("order_123.pdf");
-        order.sendEmailNotification("johndoe@example.com");
+        order.sendEmailNotification("edroso@example.com");
     }
 }
